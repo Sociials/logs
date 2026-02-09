075c24e4e14c7b9d05c822685c9264f5a98aa9f1
Nishu-workspace <nishupatel25112003@gmail.com>
2026-02-09 16:59:05 +0530

    removed bgs

--- app/components/AsciiBackground.tsx
+++ app/components/AsciiBackground.tsx
@@ -3,38 +3,9 @@
 export default function AsciiBackground() {
   return (
     <div className="ascii-bg" aria-hidden="true">
-      {/* Top-right corner decoration */}
-      <pre className="ascii-art top-right">
-        {`        ·  ·  ·  ·  ·  ·
-      ·  ·  ·  ·  ·  ·  ·
-    ·  ·  ·  ·  ·  ·  ·  ·
-  ·  ·  ·  ·  ·  ·  ·  ·  ·
-·  ·  ·  ·  ·  ·  ·  ·  ·  ·`}
-      </pre>
 
-      {/* Bottom-left corner decoration */}
-      <pre className="ascii-art bottom-left">
-        {`·  ·  ·  ·  ·  ·  ·  ·  ·  ·
-  ·  ·  ·  ·  ·  ·  ·  ·  ·
-    ·  ·  ·  ·  ·  ·  ·  ·
-      ·  ·  ·  ·  ·  ·  ·
-        ·  ·  ·  ·  ·  ·`}
-      </pre>
 
-      {/* Center-right subtle pattern */}
-      <pre className="ascii-art center-right">
-        {`│
-│
-│
-│
-│
-│
-│
-│`}
-      </pre>
 
-      {/* Subtle grid pattern */}
-      <div className="grid-pattern"></div>
     </div>
   );
 }
